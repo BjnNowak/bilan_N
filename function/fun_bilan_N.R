b607fc2757fec2e93d57bd779eb9cbc30db46eca
BjnNowak <83442466+BjnNowak@users.noreply.github.com>
2025-03-28 14:16:08 +0100

    Add files via upload

--- function/fun_bilan_N.R
+++ function/fun_bilan_N.R
@@ -1,18 +1,4 @@
-library(tidyverse)
-
-data<-tibble(
-  plot=c("test"),          # name of the plot
-  crop=c("Ble"),         # type of crop
-  cultivar=c("Apache"),    # cultivar name (only for wheat)
-  yield=c(80),             # yield target (numeric variable)
-  tillers=c(2),            # number of tillers
-  soil=c("Terres noires"), # soil type
-  previous=c("Mais grain"),      # previous crop
-  residues=c("Enfouis")   # crop residues management
-)           
-
-data
-
+# Function to compute nitrogen budget
 bilan_N<-function(data){
   balance <- data%>%
     left_join(
@@ -38,34 +24,8 @@ bilan_N<-function(data){
     mutate(Pf=yield*b)%>%
     mutate(Pi=10+5*tillers)%>%
     select(plot,Pf,Rf,Pi,Ri,Mh,Mr)%>%
+    # Compute amount of nitrogen
     mutate(X=Pf+Rf-(Pi+Ri+Mh+Mr))
   
   return(balance)
 }
-
-bilan_N(data)
-
-data_2<-tibble(
-  plot=c("test","test2"),          # name of the plot
-  crop=c("Ble","Ble"),         # type of crop
-  cultivar=c("Apache","Uli 12"),    # cultivar name (only for wheat)
-  yield=c(80,80),             # yield target (numeric variable)
-  tillers=c(2,1),            # number of tillers
-  soil=c("Terres noires","Alluvions"), # soil type
-  previous=c("Mais grain","Colza"),      # previous crop
-  residues=c("Enfouis","Enfouis")   # crop residues management
-) 
-
-bilan_N(data_2)
-
-
-tst<-data%>%
-  left_join(
-    read_delim('https://github.com/BjnNowak/bilan_N/raw/refs/heads/main/data/Mh.csv',delim=";"),
-    by=c('soil','residues')         
-  )
-
-tst<-read_delim('https://github.com/BjnNowak/bilan_N/raw/refs/heads/main/data/Mh.csv',delim=";")
-tst
-tst$soil
-tst$previous
